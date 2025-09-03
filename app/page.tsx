607cb3277e813db3a294f036848dbbc8407c6a42
Rahul Yadav2 <rahulyadav2@virtualemployee.com>
2025-09-03 18:54:40 +0530

    page change

--- app/page.tsx
+++ app/page.tsx
@@ -14,9 +14,10 @@ export default function Home() {
         />
         <ol className="font-mono list-inside list-decimal text-sm/6 text-center sm:text-left">
           <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
+          Demo
             <code className="bg-black/[.05] dark:bg-white/[.06] font-mono font-semibold px-1 py-0.5 rounded">
-              app/page.tsx
+              Headelss
+              
             </code>
             .
           </li>
